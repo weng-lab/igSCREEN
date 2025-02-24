f580d23b7233e15b07cf7579bd5c59cb398879c9
jpfisher72 <jpfisher72@gmail.com>
2025-02-24 10:30:03 -0500

    Cleanup dead code

--- immuscreen/src/common/lib/themes.ts
+++ immuscreen/src/common/lib/themes.ts
@@ -1,7 +1,6 @@
 'use client'
 import { createTheme } from "@mui/material/styles"
 
-// temp theme for toolbar color and accordion outline - UMass blue / empty secondary
 export const defaultTheme = createTheme({
   palette: {
     mode: "light",
