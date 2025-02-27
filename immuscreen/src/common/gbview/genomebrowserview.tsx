1293f89c5e4d1ad58ff2a3cc144209a26efc0a5e
Jair Meza <jair3202@gmail.com>
2025-02-27 15:13:09 -0500

    Added GB functionality

--- immuscreen/src/common/gbview/genomebrowserview.tsx
+++ immuscreen/src/common/gbview/genomebrowserview.tsx
@@ -1,17 +1,16 @@
 "use client"
-import React, { useMemo, useState, useRef, useCallback, useEffect } from "react"
+import React, { useMemo, useState, useEffect, useRef } from "react"
 import Grid2 from "@mui/material/Grid2"
-import { RulerTrack, GenomeBrowser } from "umms-gb"
-import Controls from "./controls"
-import { gql, useQuery } from "@apollo/client"
-import CytobandView, { GenomicRange } from "./cytobandview"
-import EGeneTracks from "./egenetracks"
-import { client } from "../utils"
-import DefaultTracks from "./defaulttracks"
-import BulkAtacTracks from "./bulkatactracks";
-import ChromBPNetAtacTracks from "./chrombpnetatactracks";
-import { Box } from "@mui/material"
-import { BrowserActionType, GenomeBrowser as NewGenomeBrowser, BrowserAction, TrackType, BrowserState, Controls as GBControls, GQLCytobands, useBrowserState, TranscriptTrackProps, TranscriptHumanVersion, TranscriptMouseVersion, DefaultTranscript, DefaultBigBed, DisplayMode, BigBedTrackProps, GQLWrapper } from '@weng-lab/genomebrowser';
+import { Box, Button, IconButton } from "@mui/material"
+import { BrowserActionType, TrackType, GQLCytobands, useBrowserState, TranscriptTrackProps, TranscriptHumanVersion, TranscriptMouseVersion, DefaultTranscript, DefaultBigBed, DisplayMode, BigBedTrackProps, GQLWrapper, DefaultBigWig, Controls, GenomeBrowser } from '@weng-lab/genomebrowser';
+import { Rect } from "umms-gb/dist/components/tracks/bigbed/types"
+import { CellQueryValue } from "../../app/celllineage/types"
+import BulkAtacModal from "./bulkAtacSelector"
+import { getCellDisplayName } from "../../app/celllineage/utils"
+import { getCellColor } from "../../app/celllineage/utils"
+import { Search } from "@mui/icons-material"
+import { GenomeSearch, Result } from "@weng-lab/psychscreen-ui-components"
+import { GenomicRange } from "./types";
 
 type GenomeBrowserViewProps = {
   coordinates: {
@@ -29,30 +28,7 @@ type GenomeBrowserViewProps = {
   },
   assembly: string
 }
-const GENE_QUERY = gql`
-  query s($chromosome: String, $start: Int, $end: Int, $assembly: String!,  $version: Int) {
-    gene(chromosome: $chromosome, start: $start, end: $end, assembly: $assembly, version :$version) {
-      name
-      strand
-      transcripts {
-        name
-        strand
-        exons {
-          coordinates {
-            chromosome
-            start
-            end
-          }
-        }
-        coordinates {
-          chromosome
-          start
-          end
-        }
-      }
-    }
-  }
-`
+
 export type Transcript = {
   id: string
   name: string
@@ -66,7 +42,7 @@ export type SNPQueryResponse = {
     transcripts: Transcript[]
   }[]
 }
-export function expandCoordinates(coordinates, l = 20000) {
+function expandCoordinates(coordinates, l = 20000) {
   return {
     chromosome: coordinates.chromosome,
     start: coordinates.start - l < 0 ? 0 : coordinates.start - l,
@@ -74,15 +50,10 @@ export function expandCoordinates(coordinates, l = 20000) {
   }
 }
 
-function randomID() {
-  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
-}
-
 export const GenomeBrowserView: React.FC<GenomeBrowserViewProps> = (props: GenomeBrowserViewProps) => {
-  const svgRef = useRef<SVGSVGElement>(null)
+  const containerRef = useRef<HTMLDivElement>(null)
   const expandedCoordinates = useMemo(() => expandCoordinates(props.coordinates), [props.coordinates])
   const [coordinates, setCoordinates] = useState<GenomicRange>(expandedCoordinates)
-  const [highlight, setHighlight] = useState(null)
 
   // Browser State
   const initialBrowserCoords = useMemo(() => {
@@ -97,36 +68,56 @@ export const GenomeBrowserView: React.FC<GenomeBrowserViewProps> = (props: Genom
     })
   }, [initialBrowserCoords.chromosome, initialBrowserCoords.end, initialBrowserCoords.start])
 
-  const initialTracks = useMemo(() => {
-    if (props.assembly) {
-      const geneTrack = {
-        ...DefaultTranscript,
-        titleSize: 16,
-        id: randomID(),
-        title: "GENCODE genes",
-        height: 100,
-        color: "#AAAAAA",
-        version: props.assembly.toLowerCase() === "mm10" ? TranscriptMouseVersion.V25 : TranscriptHumanVersion.V40,
-        assembly: props.assembly,
-        queryType: "gene",
-      } as TranscriptTrackProps
+  const bedMouseOver = (item: Rect) => {
+    const newHighlight = { domain: { start: item.start + 150, end: item.end + 150 }, color: item.color || "red", id: item.name }
+    browserDispatch({ type: BrowserActionType.ADD_HIGHLIGHT, highlight: newHighlight })
+  }
+  const bedMouseOut = () => {
+    browserDispatch({ type: BrowserActionType.REMOVE_LAST_HIGHLIGHT })
+  }
 
-      const icreTrack =
-        {
-          ...DefaultBigBed,
-          titleSize: 16,
-          id: "default-icre",
-          title: "All Immune cCres",
-          displayMode: DisplayMode.DENSE,
-          color: "#FFFFFF",
-          rowHeight: 12,
-          height: 50,
-          url: "https://downloads.wenglab.org/Calderon-Corces_activeCREs_iSCREEN_withcolors.bigBed"
-        } as BigBedTrackProps
-      return (
-        [geneTrack, icreTrack]
-      )
-    } else return []
+  const initialTracks = useMemo(() => {
+    if (!props.assembly) return []
+    const geneTrack = {
+      ...DefaultTranscript,
+      titleSize: 16,
+      id: "default-gene",
+      title: "GENCODE genes",
+      height: 100,
+      color: "#AAAAAA",
+      version: props.assembly.toLowerCase() === "mm10" ? TranscriptMouseVersion.V25 : TranscriptHumanVersion.V47,
+      assembly: props.assembly,
+      queryType: "gene",
+      displayMode: DisplayMode.SQUISH,
+      geneName: props.gene
+    } as TranscriptTrackProps
+
+    const icreTrack = {
+      ...DefaultBigBed,
+      titleSize: 16,
+      id: "default-icre",
+      title: "All Immune cCres",
+      displayMode: DisplayMode.DENSE,
+      color: "#9378bc",
+      rowHeight: 20,
+      height: 75,
+      onMouseOver: bedMouseOver,
+      onMouseOut: bedMouseOut,
+      url: "https://downloads.wenglab.org/Calderon-Corces_activeCREs_iSCREEN_withcolors.bigBed"
+    } as BigBedTrackProps
+
+    const allImmuneBigWig = {
+      ...DefaultBigWig,
+      title: "All Immune Cells (Aggregate Signal)",
+      url: "https://downloads.wenglab.org/all_immune.bigWig",
+      color: "#000000",
+      height: 75,
+      rowHeight: 12,
+      displayMode: DisplayMode.FULL,
+      id: "all-immune-bigwig"
+    }
+
+    return [geneTrack, icreTrack, allImmuneBigWig]
   }, [props.assembly])
 
   const initialBrowserState = useMemo(() => {
@@ -165,110 +156,118 @@ export const GenomeBrowserView: React.FC<GenomeBrowserViewProps> = (props: Genom
     }
   }, [browserDispatch, browserInitialized, browserState.tracks, coordinates, initialDomain, initialTracks, props.coordinates, props.gene]);
 
-  const snpResponse = useQuery<SNPQueryResponse>(GENE_QUERY, {
-    variables: { ...coordinates, assembly: props.assembly, version: 40 },
-    fetchPolicy: "cache-and-network",
-    nextFetchPolicy: "cache-first",
-    client,
-  })
+  // Bulk ATAC Modal
+  const [settingsModalShown, setSettingsModalShown] = useState(false)
+  const [selectedCells, setSelectedCells] = useState<CellQueryValue[]>([])
 
-  const groupedTranscripts = useMemo(
-    () =>
-      snpResponse.data?.gene.map((x) => ({
-        ...x,
-        transcripts: x.transcripts.map((xx) => ({
-          ...xx,
-          color: props.gene ? (x.name.includes(props.gene) ? "#880000" : "#aaaaaa") : "#aaaaaa",
-        })),
-      })),
-    [snpResponse, props.gene]
-  )
-  const onDomainChanged = useCallback(
-    (d: GenomicRange) => {
-      const chr = d.chromosome === undefined ? props.coordinates.chromosome : d.chromosome
-      const start = Math.round(d.start)
-      const end = Math.round(d.end)
-      if (end - start > 10) {
-        setCoordinates({ chromosome: chr, start, end })
+  useEffect(() => {
+    // Remove only bulk ATAC tracks for deselected cells
+    console.log(selectedCells)
+    browserState.tracks.forEach(track => {
+      if (track.id === "all-immune-bigwig" || track.id === "default-icre" || track.id === "default-gene") return
+      if (!selectedCells.some(cell => cell === track.id)) {
+        console.log("deleting", track.id)
+        browserDispatch({ type: BrowserActionType.DELETE_TRACK, id: track.id })
       }
-    },
-    [props.coordinates]
-  )
-  const l = useCallback((c) => ((c - coordinates.start) * 1400) / (coordinates.end - coordinates.start), [coordinates])
+    })
+    // Add tracks for selected cells
+    const x: [string, string, string][] = selectedCells.map(cell => {
+      return ([
+        getCellDisplayName(cell, true, true) + (["HSC", "CD34_Cord_Blood", "CD34_Bone_Marrow"].find(x => x === cell) ? ` (${cell})` : ''),
+        `https://downloads.wenglab.org/${cell}.bigWig`,
+        getCellColor(cell)
+      ])
+    }) || []
+    x.map((cell, index) => {
+      if (!browserState.tracks.find(t => t.id === selectedCells[index])) {
+        const track = {
+          ...DefaultBigWig,
+          title: cell[0],
+          url: cell[1],
+          color: cell[2],
+          height: 75,
+          displayMode: DisplayMode.FULL,
+          id: selectedCells[index]
+        }
+        console.log(track)
+        browserDispatch({ type: BrowserActionType.ADD_TRACK, track })
+      }
+    })
+  }, [selectedCells, browserState.tracks])
+
+  const handeSearchSubmit = (r: Result) => {
+    console.log(r)
+  }
 
   return (
-    <>
-      <Grid2 container spacing={3} sx={{ mt: "1rem", mb: "1rem" }}>
-        <Grid2
-          size={{
-            xs: 12,
-            lg: 12
-          }}>
-          <br />
-          <CytobandView innerWidth={1000} height={15} chromosome={coordinates.chromosome!} assembly={"hg38"} position={coordinates} />
-          <br />
-          <div style={{ textAlign: "center" }}>
-            <Controls onDomainChanged={onDomainChanged} domain={coordinates || props.coordinates} />
-          </div>
-          <br />
-          <br />
-          <Box id="GB">
-            <GenomeBrowser
-              svgRef={svgRef}
-              domain={coordinates}
-              innerWidth={1400}
-              width="100%"
-              noMargin
-              onDomainChanged={(x) => {
-                if (Math.ceil(x.end) - Math.floor(x.start) > 10) {
-                  setCoordinates({
-                    chromosome: coordinates.chromosome,
-                    start: Math.floor(x.start),
-                    end: Math.ceil(x.end),
-                  })
-                }
-              }}
-            >
-              {highlight && (
-                <rect fill="#8ec7d1" fillOpacity={0.5} height={'100%'} x={l(highlight.start)} width={l(highlight.end) - l(highlight.start)} />
-              )}
-              <RulerTrack domain={coordinates} height={30} width={1400} />
-              <>
-                {props.accession && <rect fill="#FAA4A4" fillOpacity={0.3} height={'100%'} x={l(props.accession.start)} width={l(props.accession.end) - l(props.accession.start)} />
-                }
-              </>
-              <EGeneTracks
-                genes={groupedTranscripts || []}
-                expandedCoordinates={coordinates}
-                squish={coordinates.end - coordinates.start >= 500000 ? true : false}
-              />
-              <DefaultTracks
-                assembly={props.assembly}
-                domain={coordinates}
-                oncCREMousedOver={(x) => x && setHighlight(x)}
-                oncCREMousedOut={() => setHighlight(null)}
-              />
-              <BulkAtacTracks
-                assembly="GRCh38"
-                domain={coordinates}
-                defaultcelltypes={props.defaultcelltypes}
-              />
-              {
-                /**
-                 * @todo Need to make sure chrombpnet tracks also include Corces 
-                 */
-              }
-              {/* <ChromBPNetAtacTracks
-            domain={coordinates}
-            defaultcelltypes={props.defaultcelltypes}
-          /> */}
-            </GenomeBrowser>
+    <GQLWrapper>
+      <Grid2 container spacing={3} sx={{ mt: "0rem", mb: "1rem" }} ref={containerRef} justifyContent="center" alignItems="center">
+        <Grid2 size={{ xs: 12, lg: 12 }} style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", marginTop: "0px" }}>
+          <Box sx={{ width: "100%", display: "flex", justifyContent: "space-between", mb: 2 }}>
+            <Button variant="contained" onClick={() => setSettingsModalShown(true)}>
+              Add ATAC Tracks
+            </Button>
+            <GenomeSearch
+              assembly="GRCh38"
+              onSearchSubmit={handeSearchSubmit}
+              queries={["gene", "snp", "icre"]}
+              geneLimit={3}
+              sx={{ width: "400px" }}
+            />
           </Box>
-          <GQLWrapper>
-            <NewGenomeBrowser width={"100%"} browserState={browserState} browserDispatch={browserDispatch} />
-          </GQLWrapper>
-
+          <BulkAtacModal
+            open={settingsModalShown}
+            onCancel={() => setSettingsModalShown(false)}
+            onAccept={(cells: CellQueryValue[]) => {
+              setSelectedCells(cells);
+              setSettingsModalShown(false)
+            }}
+            selected={selectedCells}
+          />
+          <h3 style={{ marginBottom: "0px", marginTop: "0px" }}>
+            {props.assembly} at {browserState.domain.chromosome}:{browserState.domain.start.toLocaleString()}-{browserState.domain.end.toLocaleString()}
+          </h3>
+          <svg id="cytobands" width={"700px"} height={20}>
+            <GQLCytobands assembly={props.assembly === "GRCh38" ? "hg38" : "mm10"} chromosome={browserState.domain.chromosome} currentDomain={browserState.domain} />
+          </svg>
         </Grid2>
-      </Grid2>
-    </>);
+        <Grid2 size={{ xs: 12, lg: 12 }}>
+          <Controls
+            inputButtonComponent={
+              <IconButton type="button" sx={{
+                color: "black",
+                maxHeight: "100%",
+                padding: "4px"
+              }}>
+                <Search fontSize="small" />
+              </IconButton>
+            }
+            buttonComponent={
+              <Button
+                variant="outlined"
+                sx={{
+                  minWidth: "0px",
+                  width: { xs: "100%", sm: "80%" },
+                  maxWidth: "120px",
+                  fontSize: "0.8rem",
+                  padding: "4px 8px"
+                }}
+              />
+            }
+            domain={browserState.domain}
+            dispatch={browserDispatch}
+            withInput={false}
+            style={{
+              paddingBottom: "4px",
+              display: "flex",
+              flexWrap: "wrap",
+              gap: "4px",
+              width: "100%"
+            }}
+          />
+          <GenomeBrowser width={"100%"} browserState={browserState} browserDispatch={browserDispatch} />
+        </Grid2>
+      </Grid2 >
+    </GQLWrapper>
+  );
 }
