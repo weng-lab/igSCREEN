efc94c35daa0728c7900fb1006aa8531a2332916
jpfisher72 <jpfisher72@gmail.com>
2025-02-28 16:55:05 -0500

    Generic Layout

--- immuscreen/src/app/snp/[rsID]/layout.tsx
+++ immuscreen/src/app/snp/[rsID]/layout.tsx
@@ -1,9 +1,5 @@
-'use client'
+import ElementDetailsLayout from "common/ElementDetails/ElementDetailsLayout"
 
-import { Stack, useMediaQuery, useTheme } from '@mui/material';
-import ElementDetailsTabs from './ElementDetailsTabs';
-import ElementDetailsBreadcrumbs from './ElementDetailsBreadcrumbs';
-import ElementDetailsHeader from './ElementDetailsHeader';
 
 export default function RegionSearchLayout({
   children,
@@ -12,35 +8,22 @@ export default function RegionSearchLayout({
   children: React.ReactNode,
   params: { rsID: string }
 }) {
-  const theme = useTheme()
-  const isDesktop = useMediaQuery(theme.breakpoints.up('md'));
-
-  const spaceBetween = 2
-
   return (
-    <Stack height={'100%'} direction={isDesktop ? 'row' : 'column'}>
-      {/* Tabs */}
-      <Stack order={isDesktop ? 1 : 2} sx={!isDesktop && { gap: spaceBetween, m: spaceBetween }}>
-        <ElementDetailsTabs
-          tabs={[
-            {
-              label: 'eQTLs',
-              href: 'eQTLs'
-            },
-            {
-              label: 'Nearby Genomic Features',
-              href: 'nearby'
-            },
-          ]}
-        />
-        {!isDesktop && children}
-      </Stack>
-      {/* Header */}
-      <Stack order={isDesktop ? 2 : 1} sx={{ gap: spaceBetween, p: spaceBetween, width: '100%' }}>
-        <ElementDetailsBreadcrumbs />
-        <ElementDetailsHeader elementName={params.rsID} elementType='SNP' />
-        {isDesktop && children}
-      </Stack>
-    </Stack>
+    <ElementDetailsLayout
+      tabs={[
+        {
+          label: 'eQTLs',
+          href: 'eQTLs'
+        },
+        {
+          label: 'Nearby Genomic Features',
+          href: 'nearby'
+        },
+      ]}
+      elementName={params.rsID}
+      elementType='SNP'
+    >
+      {children}
+    </ElementDetailsLayout>
   )
 }
--- immuscreen/src/common/ElementDetails/ElementDetailsLayout.tsx
+++ immuscreen/src/common/ElementDetails/ElementDetailsLayout.tsx
@@ -5,15 +5,9 @@ import ElementDetailsBreadcrumbs from './ElementDetailsBreadcrumbs';
 import ElementDetailsTabs, { ElementDetailsTabsProps } from './ElementDetailsTabs';
 import ElementDetailsHeader, { ElementDetailsHeaderProps } from './ElementDetailsHeader';
 
-export type ElementDetailsLayoutProps = ElementDetailsTabsProps & ElementDetailsHeaderProps
+export type ElementDetailsLayoutProps = ElementDetailsTabsProps & ElementDetailsHeaderProps & {children: React.ReactNode}
 
-export default function ElementDetailsLayout({
-  props,
-  children,
-}: {
-  props: ElementDetailsLayoutProps
-  children: React.ReactNode,
-}) {
+export default function ElementDetailsLayout({tabs, elementName, elementType, children}: ElementDetailsLayoutProps) {
   const theme = useTheme()
   const isDesktop = useMediaQuery(theme.breakpoints.up('md'));
   const spaceBetween = 2
@@ -22,13 +16,13 @@ export default function ElementDetailsLayout({
     <Stack height={'100%'} direction={isDesktop ? 'row' : 'column'}>
       {/* Tabs */}
       <Stack order={isDesktop ? 1 : 2} sx={!isDesktop && { gap: spaceBetween, m: spaceBetween }}>
-        <ElementDetailsTabs tabs={props.tabs} />
+        <ElementDetailsTabs tabs={tabs} />
         {!isDesktop && children}
       </Stack>
       {/* Header */}
       <Stack order={isDesktop ? 2 : 1} sx={{ gap: spaceBetween, p: spaceBetween, width: '100%' }}>
         <ElementDetailsBreadcrumbs />
-        <ElementDetailsHeader elementName={props.elementName} elementType={props.elementType} />
+        <ElementDetailsHeader elementName={elementName} elementType={elementType} />
         {isDesktop && children}
       </Stack>
     </Stack>
