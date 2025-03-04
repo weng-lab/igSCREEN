3755aaa2bf37021df0acecc457147d772229c8b5
Jair Meza <jair3202@gmail.com>
2025-03-04 15:45:23 -0500

    GenomeBrowser input fix
    The GenomeSearch component in the genome browser view now sets the genome browser coordinates instead of loading a new URL

--- src/common/components/mainsearch/autocomplete.tsx
+++ src/common/components/mainsearch/autocomplete.tsx
@@ -1,36 +1,39 @@
-'use client'
-import { GenomeSearch, GenomeSearchProps, Result } from "@weng-lab/psychscreen-ui-components";
+"use client";
+import {
+  GenomeSearch,
+  GenomeSearchProps,
+  Result,
+} from "@weng-lab/psychscreen-ui-components";
 import { useRouter } from "next/navigation";
 
 export default function AutoComplete(props: Partial<GenomeSearchProps>) {
-    const router = useRouter()
+  const router = useRouter();
 
-    const handleSearchSubmit = (r: Result) => {
-        let url = ""
-        switch (r.type) {
-            case "gene":
-                url = `/gene/${r.title}`
-                break;
-            case "icre":
-                url = `/icre/${r.title}`
-                break;
-            case "coordinate":
-                url = `/region/${r.domain.chromosome}:${r.domain.start}-${r.domain.end}`
-                break;
-            case "snp":
-                url = `/snp/${r.title}`
-                break;
-        }
-        router.push(url)
+  const handleSearchSubmit = (r: Result) => {
+    let url = "";
+    switch (r.type) {
+      case "gene":
+        url = `/gene/${r.title}`;
+        break;
+      case "icre":
+        url = `/icre/${r.title}`;
+        break;
+      case "coordinate":
+        url = `/region/${r.domain.chromosome}:${r.domain.start}-${r.domain.end}`;
+        break;
+      case "snp":
+        url = `/snp/${r.title}`;
+        break;
     }
+    router.push(url);
+  };
 
-    return (
-        <GenomeSearch
-            {...props}
-            assembly="GRCh38"
-            onSearchSubmit={handleSearchSubmit}
-            queries={["gene", "icre", "snp", "coordinate"]}
-            
-        />
-    )
+  return (
+    <GenomeSearch
+      assembly="GRCh38"
+      onSearchSubmit={handleSearchSubmit}
+      queries={["gene", "icre", "snp", "coordinate"]}
+      {...props}
+    />
+  );
 }
