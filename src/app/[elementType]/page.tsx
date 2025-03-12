c23bbbaf4cd66b0a6069edadd92364acd6916202
JaiiR320 <jair3202@gmail.com>
2025-03-12 11:41:31 -0400

    Update page.tsx

--- src/app/[elementType]/page.tsx
+++ src/app/[elementType]/page.tsx
@@ -26,7 +26,7 @@ export default function PortalPage({ params: {elementType} }: { params: { elemen
       <GenomeSearch
         assembly="GRCh38"
         onSearchSubmit={handleSearchSubmit}
-        queries={[elementType == "gene" ? "Gene" : elementType === "icre" ? "iCRE" : "SNP"]}
+        queries={[elementType == "gene" ? "Gene" : elementType === "icre" ? "iCRE" : "SNP"]} // TODO: Add more queries
         fullWidth
       />
     </Stack>
