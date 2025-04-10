cd55cb293cde7394b09c0e11b945a1ed9f69fa26
jpfisher72 <jpfisher72@gmail.com>
2025-04-10 14:01:56 -0400

    Additional Cleanup

--- src/common/utility.tsx
+++ src/common/utility.tsx
@@ -96,3 +96,51 @@ export const ExternalLink: React.FC<LinkProps & { showExternalIcon?: boolean }> 
     </Link>
   )
 }
+
+export function toScientificNotation(num: number, sigFigs?: number) {
+  // Convert the number to scientific notation using toExponential
+  let scientific = num.toExponential(sigFigs ?? undefined);
+  
+  // Split the scientific notation into the coefficient and exponent parts
+  let [coefficient, exponent] = scientific.split('e');
+  
+  // Format the exponent part
+  let expSign = exponent[0];
+  exponent = exponent.slice(1);
+  
+  // Convert the exponent to a superscript string
+  let superscriptExponent = exponent
+    .split('')
+    .map(char => '⁰¹²³⁴⁵⁶⁷⁸⁹'[char] || char)
+    .join('');
+  
+  // Add the sign back to the exponent
+  superscriptExponent = (expSign === '-' ? '⁻' : '') + superscriptExponent;
+  
+  // Combine the coefficient with the superscript exponent
+  return coefficient + '×10' + superscriptExponent;
+}
+
+const svgData = (_svg): string => {
+  let svg = _svg.cloneNode(true);
+  svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+  let preface = '<?xml version="1.0" standalone="no"?>';
+  return preface + svg.outerHTML.replace(/\n/g, '').replace(/[ ]{8}/g, '');
+};
+
+const downloadData = (text: string, filename: string, type: string = 'text/plain') => {
+  const a = document.createElement('a');
+  document.body.appendChild(a);
+  a.setAttribute('style', 'display: none');
+  const blob = new Blob([text], { type });
+  const url = window.URL.createObjectURL(blob);
+  a.href = url;
+  a.download = filename;
+  a.click();
+  window.URL.revokeObjectURL(url);
+  a.remove();
+};
+
+export const downloadSVG = (ref: React.MutableRefObject<SVGSVGElement>, filename: string) => {
+  ref.current && downloadData(svgData(ref.current!), filename, 'image/svg;charset=utf-8');
+}
