8784d832091e88639ac86cf440c4624497407a51
jpfisher72 <jpfisher72@gmail.com>
2025-04-10 14:10:24 -0400

    Change back

--- src/app/lineage/page.tsx
+++ src/app/lineage/page.tsx
@@ -21,7 +21,7 @@ import {
 } from "@mui/material";
 import CellLineageTree, { cellTypeConfig, getCellImagePath, NodeInfo } from "common/components/CellLineageTree";
 import React, { useCallback, useMemo, useRef, useState } from "react";
-import MultiSelect from "./MultiSelector";
+import MultiSelect from "./MultiSelect";
 import Image from "next/image";
 import { BarChartOutlined, Close, Download, Info, NavigateNext, Sync } from "@mui/icons-material";
 import { gql } from "types/generated";
