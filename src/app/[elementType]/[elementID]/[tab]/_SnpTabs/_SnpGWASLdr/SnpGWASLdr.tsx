132db8e8eea544af1d0c3bdca976aa9a3fa32cb1
Jonathan Fisher <49597360+jpfisher72@users.noreply.github.com>
2025-04-14 14:38:50 -0400

    Fix Various Bugs (#110)
    * Fix #108
    
    * Fix #107
    
    * Quick fix for #109

--- src/app/[elementType]/[elementID]/[tab]/_SnpTabs/_SnpGWASLdr/SnpGWASLdr.tsx
+++ src/app/[elementType]/[elementID]/[tab]/_SnpTabs/_SnpGWASLdr/SnpGWASLdr.tsx
@@ -1,104 +1,102 @@
-import { Grid2 as Grid, Skeleton, Typography } from "@mui/material";
+import { Box, Grid2 as Grid, Skeleton, Typography } from "@mui/material";
 import useGWASLdr from "common/hooks/useGWASLdr";
 import DataGridToolbar from "common/components/dataGridToolbar";
-import { DataGridPro, GridColDef } from "@mui/x-data-grid-pro";
+import { DataGridPro, gridClasses, GridColDef } from "@mui/x-data-grid-pro";
 import { Link as MuiLink } from "@mui/material";
 import Link from "next/link";
+import { LinkComponent } from "common/utility";
 
 export default function SnpGWASLdr({ snpid }: { snpid: string }) {
   const { data, loading, error } = useGWASLdr(undefined, [snpid]);
 
+  const cols: GridColDef[] = [
+    {
+      field: "icre",
+      headerName: "Accession",
+      width: 150,
+      renderCell: (params) => (
+        <LinkComponent href={"/icre/" + params.value} underline="hover">
+          {params.value}
+        </LinkComponent>
+      ),
+    },
+    {
+      field: "ref_allele",
+      headerName: "Ref Allele",
+    },
+    {
+      field: "effect_allele",
+      headerName: "Effect Allele",
+    },
+    {
+      field: "zscore",
+      headerName: "Z-score",
+      type: 'number',
+      valueFormatter: (value?: number) => {
+        if (value == null) {
+          return '';
+        }
+        return `${value.toFixed(2)}`;
+      },
+    },
+    {
+      field: "disease",
+      headerName: "Disease",
+      width: 200,
+      valueGetter: (value, row) => {
+        return value === "" ? row.study_source : value
+      },
+    },
+    {
+      field: "study_source",
+      headerName: "Source",
+    },
+  ];
+
   return (
-    <Grid container spacing={2}>
-      <Grid size={12}>
-        {loading ? (
-          <Skeleton variant="rounded" width={"100%"} height={100} />
-        ) : data.length > 0 ? (
-          <>
-            <DataGridPro
-              rows={data || []}
-              columns={
-                [
-                  {
-                    field: "icre",
-                    headerName: "Accession",
-                    flex: 1,
-                    renderCell: (params) => {
-                      return params.value ? (
-                        <MuiLink component={Link} href={"/icre/" + params.value}>
-                          <i>{params.value}</i>
-                        </MuiLink>
-                      ) : (
-                        "."
-                      );
-                    },
-                  },
-                  {
-                    field: "ref_allele",
-                    headerName: "Ref Allele",
-                    flex: 1,
-                  },
-                  {
-                    field: "effect_allele",
-                    headerName: "Effect Allele",
-                    flex: 1,
-                  },
-                  {
-                    field: "zscore",
-                    headerName: "Z-score",
-                    flex: 1,
-                    renderCell: (params) => {
-                      return params.value.toFixed(2);
-                    },
-                  },
-                  {
-                    field: "disease",
-                    headerName: "Disease",
-                    flex: 1,
-                    renderCell: (params) => {
-                      return params.value == "" ? params.row.study_source : params.value;
-                    },
-                  },
-                  {
-                    field: "study_source",
-                    headerName: "Source",
-                    flex: 1,
-                  },
-                ] as GridColDef[]
-              }
-              getRowId={(row) => row.zscore + row.study}
-              pagination
-              initialState={{
-                sorting: {
-                  sortModel: [{ field: "zscore", sort: "desc" }],
-                },
-                pagination: {
-                  paginationModel: { pageSize: 10 },
-                },
-              }}
-              slots={{ toolbar: DataGridToolbar }}
-              slotProps={{ toolbar: { title: "GWAS Ldr" } }}
-              density="compact"
-              sx={{ width: "100%", height: "auto" }}
-              style={{ boxShadow: "0px 6px 12px rgba(0,0,0,0.2)" }}
-            />
-          </>
-        ) : (
-          <Typography
-            variant="h6"
-            pl={1}
-            sx={{
-              border: "1px solid #e0e0e0",
-              borderRadius: 1,
-              p: 2,
-              boxShadow: "0px 2px 4px rgba(0,0,0,0.1)",
-              marginBottom: 2,
-            }}
-          >
-            No GWAS ldr data found
-          </Typography>
-        )}
-      </Grid>
-    </Grid>
+    <Box width={"100%"}>
+      {loading ? (
+        <Skeleton variant="rounded" width={"100%"} height={100} />
+      ) : data.length > 0 ? (
+        <DataGridPro
+          rows={data || []}
+          columns={cols.map((col) => {
+            return { ...col, display: "flex" };
+          })}
+          getRowId={(row) => row.zscore + row.study}
+          pagination
+          initialState={{
+            sorting: {
+              sortModel: [{ field: "zscore", sort: "desc" }],
+            },
+            pagination: {
+              paginationModel: { pageSize: 10 },
+            },
+          }}
+          slots={{ toolbar: DataGridToolbar }}
+          slotProps={{ toolbar: { title: "GWAS Ldr" } }}
+          getRowHeight={() => 'auto'}
+          sx={{
+            [`& .${gridClasses.cell}`]: {
+              py: 1,
+            },
+          }}
+          disableRowSelectionOnClick
+        />
+      ) : (
+        <Typography
+          variant="h6"
+          pl={1}
+          sx={{
+            border: "1px solid #e0e0e0",
+            borderRadius: 1,
+            p: 2,
+            marginBottom: 2,
+          }}
+        >
+          No GWAS LDR data found
+        </Typography>
+      )}
+    </Box>
   );
 }
