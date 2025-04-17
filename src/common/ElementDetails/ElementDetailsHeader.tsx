51121e4484778998a31b5b33bc81c7eb2ffbc7ca
mlacadie <lacadiem14@gmail.com>
2025-04-17 10:26:35 -0400

    card links

--- src/common/ElementDetails/ElementDetailsHeader.tsx
+++ src/common/ElementDetails/ElementDetailsHeader.tsx
@@ -38,27 +38,34 @@ const ElementDetailsHeader = ({ elementType, elementID }: ElementDetailsHeaderPr
           {loading ? <Skeleton width={215} /> : coordinatesDisplay}
         </Typography>
       </Stack>
-      <Grid2 container>
+      <Grid2 container spacing={2}>
         <Grid2 size={6}>
-        <Button
-          variant="contained"
-          href={elementID ? "https://www.genecards.org/cgi-bin/carddisp.pl?gene=" + elementID : undefined}
-          target="_blank"
-          rel="noopener noreferrer"
-          sx={{ minWidth: 125, minHeight: 50, backgroundColor: "white" }}
-        >
-          <Image style={{ objectFit: "contain" }} src="https://geneanalytics.genecards.org/media/81632/gc.png" fill alt="gene-card-button" />
-        </Button>
+          <Button
+            variant="contained"
+            href={elementID ? `http://screen.wenglab.org/search/?q=${elementID}&uuid=0&assembly=GRCh38` : undefined}
+            target="_blank"
+            rel="noopener noreferrer"
+            sx={{ minWidth: 125, height: "100%", backgroundColor: "white" }}
+          >
+            <Image style={{ objectFit: "contain", padding: 4 }} src="/SCREEN_logo_light_large.png" fill alt="screen-card-button" />
+          </Button>
         </Grid2>
-        <Button
-          variant="contained"
-          href={elementID ? "https://www.genecards.org/cgi-bin/carddisp.pl?gene=" + elementID : undefined}
-          target="_blank"
-          rel="noopener noreferrer"
-          sx={{ minWidth: 125, minHeight: 50, backgroundColor: "white" }}
-        >
-          <Image style={{ objectFit: "contain" }} src="https://geneanalytics.genecards.org/media/81632/gc.png" fill alt="gene-card-button" />
-        </Button>
+        <Grid2 size={6} display={elementType === "icre" && "none"}>
+          <Button
+            variant="contained"
+            href={
+              elementID ?
+              elementType === "gene" ?
+              "https://www.genecards.org/cgi-bin/carddisp.pl?gene=" + elementID 
+              : `https://www.ncbi.nlm.nih.gov/snp/${elementID}`
+              : undefined}
+            target="_blank"
+            rel="noopener noreferrer"
+            sx={{ minWidth: 125, height: "100%", backgroundColor: "white" }}
+          >
+            <Image style={{ objectFit: "contain" }} src={elementType === "gene" ? "https://geneanalytics.genecards.org/media/81632/gc.png" : "https://www.ncbi.nlm.nih.gov/core/assets/style-guide/img/NLM-square-logo.png"} fill alt="genecard-snpcard-button" />
+          </Button>
+        </Grid2>
       </Grid2>
     </Stack>
   )
