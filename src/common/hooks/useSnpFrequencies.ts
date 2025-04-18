0fc2debf87621a7190fa8e43e959eceab047cab4
NishiPhalke <nishaphalke@gmail.com>
2025-04-18 08:30:44 -0400

    fix snp ref alt logic

--- src/common/hooks/useSnpFrequencies.ts
+++ src/common/hooks/useSnpFrequencies.ts
@@ -43,7 +43,7 @@ export function useSnpFrequencies(rsid: string, elementType: GenomicElementType 
 
         const allele = data["mappings"][0]["allele_string"].split("/");
         const ref = allele[0];
-        const alt = allele[allele.length - 1];
+        const alt = allele.slice(1).join(",");
 
         const pop = [
           "1000GENOMES:phase_3:AMR",
