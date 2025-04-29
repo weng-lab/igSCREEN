0869f2f0a92fb5174892965d708de0773c5ba761
jpfisher72 <jpfisher72@gmail.com>
2025-04-29 16:25:52 -0400

    iCRE Activity, Gene Expression

--- src/app/[elementType]/[elementID]/[[...tab]]/_GeneTabs/_GeneExpression/GeneExpressionTable.tsx
+++ src/app/[elementType]/[elementID]/[[...tab]]/_GeneTabs/_GeneExpression/GeneExpressionTable.tsx
@@ -1,152 +1,145 @@
-import { GeneExpressionProps, PointMetadata, SharedGeneExpressionPlotProps } from "./GeneExpression"
-import { IconButton, Link } from "@mui/material"
-import { getCellCategoryDisplayname, getStudyLink } from "common/utility"
-import { GridColDef, gridFilteredSortedRowEntriesSelector, GridRowSelectionModel, GridToolbar, useGridApiRef, GRID_CHECKBOX_SELECTION_COL_DEF, DataGridPro } from "@mui/x-data-grid-pro"
-import { OpenInNew } from "@mui/icons-material"
-import { Dispatch, SetStateAction } from "react"
+import { GeneExpressionProps, PointMetadata, SharedGeneExpressionPlotProps } from "./GeneExpression";
+import { IconButton, Link, Typography } from "@mui/material";
+import { getCellCategoryDisplayname, getStudyLink } from "common/utility";
+import {
+  gridFilteredSortedRowEntriesSelector,
+  GridRowSelectionModel,
+  useGridApiRef,
+  GRID_CHECKBOX_SELECTION_COL_DEF,
+} from "@mui/x-data-grid-pro";
+import { OpenInNew } from "@mui/icons-material";
+import { Dispatch, SetStateAction } from "react";
+import CustomDataGrid, { CustomDataGridColDef } from "common/components/CustomDataGrid";
 
-export type GeneExpressionTableProps = 
-GeneExpressionProps & 
-SharedGeneExpressionPlotProps &
-{
-  onSelectionChange: (selected: PointMetadata[]) => void,
-  setSortedFilteredData: Dispatch<SetStateAction<PointMetadata[]>>
-}
+export type GeneExpressionTableProps = GeneExpressionProps &
+  SharedGeneExpressionPlotProps & {
+    onSelectionChange: (selected: PointMetadata[]) => void;
+    setSortedFilteredData: Dispatch<SetStateAction<PointMetadata[]>>;
+  };
 
-const GeneExpressionTable = ({name, id, selected, onSelectionChange, geneExpressionData, setSortedFilteredData, sortedFilteredData}: GeneExpressionTableProps) => {
-  const { data, loading, error } = geneExpressionData
+const GeneExpressionTable = ({
+  name,
+  id,
+  selected,
+  onSelectionChange,
+  geneExpressionData,
+  setSortedFilteredData,
+  sortedFilteredData,
+}: GeneExpressionTableProps) => {
+  const { data, loading, error } = geneExpressionData;
 
   //This is used to prevent sorting from happening when clicking on the header checkbox
-  const StopPropagationWrapper = (params) =>
-    <div id={'StopPropagationWrapper'} onClick={(e) => e.stopPropagation()}>
+  const StopPropagationWrapper = (params) => (
+    <div id={"StopPropagationWrapper"} onClick={(e) => e.stopPropagation()}>
       <GRID_CHECKBOX_SELECTION_COL_DEF.renderHeader {...params} />
     </div>
+  );
 
-  // ensure that "field" is accessing a true property of the row
-  type TypeSafeColDef<T> = GridColDef & { field: keyof T }; 
-
-  const columns: TypeSafeColDef<PointMetadata>[] = [
+  const columns: CustomDataGridColDef<PointMetadata>[] = [
     {
-      ...GRID_CHECKBOX_SELECTION_COL_DEF as TypeSafeColDef<PointMetadata>, //Override checkbox column https://mui.com/x/react-data-grid/row-selection/#custom-checkbox-column 
-      width: 60,
+      ...(GRID_CHECKBOX_SELECTION_COL_DEF as CustomDataGridColDef<PointMetadata>), //Override checkbox column https://mui.com/x/react-data-grid/row-selection/#custom-checkbox-column
       sortable: true,
       hideable: false,
-      renderHeader: StopPropagationWrapper
+      renderHeader: StopPropagationWrapper,
     },
     {
-      field: 'biosample',
-      headerName: 'Biosample',
-      width: 200,
+      field: "biosample",
+      headerName: "Biosample",
     },
     {
-      field: 'value',
-      headerName: 'TPM',
-      type: 'number',
-      width: 100,
-      valueGetter: (_, row) => row.value.toFixed(1)
+      field: "value",
+      headerName: "TPM",
+      type: "number",
+      valueGetter: (_, row) => row.value.toFixed(1),
     },
     {
-      field: 'stimulation',
-      headerName: 'Stimulation',
-      width: 80,
-      valueGetter: (_, row) => row.stimulation.charAt(0) === "u" ? "Unstim" : "Stim"
+      field: "stimulation",
+      headerName: "Stimulation",
+      valueGetter: (_, row) => (row.stimulation.charAt(0) === "u" ? "Unstim" : "Stim"),
     },
     {
-      field: 'lineage',
-      headerName: 'Lineage',
-      width: 150,
-      valueGetter: (_, row) => getCellCategoryDisplayname(row.lineage)
+      field: "lineage",
+      headerName: "Lineage",
+      valueGetter: (_, row) => getCellCategoryDisplayname(row.lineage),
     },
     {
-      field: 'link',
-      headerName: 'Experiment',
-      width: 80,
+      field: "link",
+      headerName: "Experiment",
       sortable: false,
       disableColumnMenu: true,
       renderCell: (params) => {
         return (
           <IconButton href={params.value} target="_blank" size="small">
-            <OpenInNew fontSize="small"/>
+            <OpenInNew fontSize="small" />
           </IconButton>
-        )
-      }
+        );
+      },
     },
     {
-      field: 'study',
-      headerName: 'Study',
-      width: 140,
+      field: "study",
+      headerName: "Study",
       renderCell: (params) => {
         return (
           <Link href={getStudyLink(params.value)} target="_blank">
             {params.value}
           </Link>
-        )
-      }
+        );
+      },
     },
   ];
 
   const handleRowSelectionModelChange = (ids: GridRowSelectionModel) => {
     const selectedRows = ids.map((id) => data.find((row) => row.name === id));
-    onSelectionChange(selectedRows)
-  }
+    onSelectionChange(selectedRows);
+  };
 
-  const apiRef = useGridApiRef()
+  const apiRef = useGridApiRef();
 
-    const arraysAreEqual = (arr1: PointMetadata[], arr2: PointMetadata[]): boolean => {
-      if (arr1.length !== arr2.length) {
-        return false
-      }
-    
-      for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i].name !== arr2[i].name) {
-          return false
-        }
-      }
-      return true
-    };
-  
-    const handleSync = () => {
-      const rows = gridFilteredSortedRowEntriesSelector(apiRef).map(x => x.model) as PointMetadata[]
-      if (!arraysAreEqual(sortedFilteredData, rows)) {
-        setSortedFilteredData(rows)
-      }
+  const arraysAreEqual = (arr1: PointMetadata[], arr2: PointMetadata[]): boolean => {
+    if (arr1.length !== arr2.length) {
+      return false;
     }
 
-  return (
-        <DataGridPro
-          apiRef={apiRef}
-          rows={data || []}
-          columns={columns.map(col => { return { ...col, display: 'flex' } })}
-          loading={loading}
-          pagination
-          initialState={{
-            pagination: {
-              paginationModel: {
-                pageSize: 10,
-              },
-            },
-            sorting: {
-              sortModel: [{field: 'value', sort: 'desc'}],
-            },
-            columns: {
-              columnVisibilityModel: {
-                study: false,
-              }
-            }
-          }}
-          sortingOrder={['desc', 'asc', null]}
-          slots={{ toolbar: GridToolbar }}
-          slotProps={{ toolbar: { showQuickFilter: true, sx: {p: 1} } }}
-          pageSizeOptions={[10, 25, 50]}
-          checkboxSelection
-          onRowSelectionModelChange={handleRowSelectionModelChange}
-          rowSelectionModel={selected.map(x => x.name)}
-          getRowId={(row) => row.name}
-          getRowHeight={() => 'auto'}
-          keepNonExistentRowsSelected //needed to prevent clearing selections on changing filters
-          onStateChange={handleSync}
-        />
-  )
-}
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i].name !== arr2[i].name) {
+        return false;
+      }
+    }
+    return true;
+  };
 
-export default GeneExpressionTable
+  const handleSync = () => {
+    const rows = gridFilteredSortedRowEntriesSelector(apiRef).map((x) => x.model) as PointMetadata[];
+    if (!arraysAreEqual(sortedFilteredData, rows)) {
+      setSortedFilteredData(rows);
+    }
+  };
+
+  return (
+    <CustomDataGrid
+      apiRef={apiRef}
+      tableTitle={
+        <Typography variant="h6">
+          <i>{name}</i> Expression
+        </Typography>
+      }
+      rows={data}
+      columns={columns}
+      loading={loading}
+      pageSizeOptions={[10, 25, 50]}
+      initialState={{
+        sorting: {
+          sortModel: [{ field: "value", sort: "desc" }],
+        },
+      }}
+      checkboxSelection
+      getRowId={(row) => row.name} //needed to match up data with the ids returned by onRowSelectionModelChange
+      onRowSelectionModelChange={handleRowSelectionModelChange}
+      rowSelectionModel={selected.map((x) => x.name)}
+      keepNonExistentRowsSelected // Needed to prevent clearing selections on changing filters
+      onStateChange={handleSync} // Not really supposed to be using this, is not documented by MUI. Not using its structure, just the callback trigger
+    />
+  );
+};
+
+export default GeneExpressionTable;
--- src/common/components/dataGridToolbar.tsx
+++ src/common/components/dataGridToolbar.tsx
@@ -1,34 +1,36 @@
 import {
   GridToolbarContainer,
-  GridToolbarDensitySelector,
   GridToolbarExport,
   GridToolbarQuickFilter,
 } from "@mui/x-data-grid-pro";
 
 import { Typography } from "@mui/material";
+import { ReactNode } from "react";
 
-export default function DataGridToolbar({ title, variant = "h6" }: { title: string, variant?: "h4" | "h6" }) {
+export type CustomDataGridToolbarProps = {
+  /**
+   * Optional ReactNode to be used in the table toolbar. Strings and numbers will be rendered as Typography variant h6.
+   */
+  title?: ReactNode;
+};
+
+export default function CustomDataGridToolbar({ title }: CustomDataGridToolbarProps) {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        paddingTop: 10,
-      }}
-    >
-      <Typography variant={variant} pl={1}>
-        {title}
-      </Typography>
-      <GridToolbarContainer>
-        <GridToolbarQuickFilter />
+    <GridToolbarContainer sx={{ p: 1, justifyContent: "space-between" }}>
+      {title && (typeof title === "string" || typeof title === "number") ? (
+        <Typography variant={"h6"}>{title}</Typography>
+      ) : (
+        title
+      )}
+      <div>
+        <GridToolbarQuickFilter sx={{ mr: 1 }} />
         <GridToolbarExport
           slotProps={{
             tooltip: { title: "Export data" },
             button: { variant: "text" },
           }}
         />
-      </GridToolbarContainer>
-    </div>
+      </div>
+    </GridToolbarContainer>
   );
 }
