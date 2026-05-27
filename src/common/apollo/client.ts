76150a8c89ab7d5b31ecbcf74dee36f5a8262f3e
mlacadie <lacadiem14@gmail.com>
2026-05-27 17:08:06 -0400

    revert

--- src/common/apollo/client.ts
+++ src/common/apollo/client.ts
@@ -12,7 +12,7 @@ export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
     link: new HttpLink({
       uri: Config.API.CcreAPI,
       headers: {
-        Authorization: "Bearer " + process.env.SHARED_API_KEY!,
+        Authorization: "Bearer " + process.env.SCREEN_API_KEY!,
       },
     }),
   });
